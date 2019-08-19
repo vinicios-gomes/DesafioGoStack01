43ee6fe45996fdfa6bada9ea2abcfc02c666cbb1
Vinicios Gomes <viniciusgomesviana@gmail.com>
2019-08-19 17:24:54 -0300

    Adicionado Rotas

--- src/index.js
+++ src/index.js
@@ -3,19 +3,57 @@ const server = express();
 
 server.use(express.json());
 
-const projects = [projec];
+const projects = [
+  {
+    id: "1",
+    title: "Projeto 01",
+    tasks: ["Tarefa"]
+  },
+  {
+    id: "2",
+    title: "Projeto 02",
+    tasks: ["tarefa 1", "tarefa 2"]
+  }
+];
 
-server.get("/projects", (req, res) => {});
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
 
-server.get("/projects/:id", (req, res) => {});
+server.post("/projects", (req, res) => {
+  const tasks = [];
+  const { id, title } = req.body;
 
-server.post("/projects", (req, res) => {});
+  projects.push({ id, title, tasks });
 
-server.put("/projects/:id", (req, res) => {});
+  return res.json(projects);
+});
 
-server.delete("/projects/:id", (req, res) => {});
+server.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
 
-server.post("/projects/:id/tasks", (req, res) => {});
+  const { title } = req.body;
+
+  projects[id].title = title;
+  return res.json(projects);
+});
+
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  projects.splice(id, 1);
+
+  return res.json(projects);
+});
+
+server.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  projects[id].tasks.push(title);
+
+  return res.json(projects);
+});
 
 server.listen(3000, () => {
   console.log("Server running on port 3000");
